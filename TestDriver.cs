051ac1825dbc954e8e6452f0fd3fdd367f13cc5a
Delrynn <delryn@patton.pro>
2024-03-07 08:31:15 -0500

    Changed test nick names

--- TestDriver.cs
+++ TestDriver.cs
@@ -40,7 +40,7 @@ namespace adiIRC_DeepL_plugin_test
             //await testPlugin.deepl_any(new RegisteredCommandArgs("_ ZZ This is a test.", fuelratsChan));
 
             // Test basic EN ratsignal
-            string rsig = "RATSIGNAL Case #3 PC HOR – CMDR IBZZ – System: \"TASCHETER SECTOR QN - T A3 - 1\" (Brown dwarf 79.9 LY from Sol) – Language: English (United Kingdom) (en-GB) (HOR_SIGNAL)";
+            string rsig = "RATSIGNAL Case #3 PC HOR – CMDR Velica_Foriana – System: \"TASCHETER SECTOR QN - T A3 - 1\" (Brown dwarf 79.9 LY from Sol) – Language: English (United Kingdom) (en-GB) (HOR_SIGNAL)";
             // Create example mecha rsig message
             ChannelNormalMessageArgs ratsignal = new ChannelNormalMessageArgs(rsig, fuelratsChan);
             ratsignal.User.Nick = "MechaSqueak[BOT]";
@@ -49,14 +49,14 @@ namespace adiIRC_DeepL_plugin_test
 
             // Check if case was onboarded into correct monitor_items slot
             if (testPlugin.monitor_items[3] != null &&
-                testPlugin.monitor_items[3].nickname.Equals("IBZZ")) testResult = true;
+                testPlugin.monitor_items[3].nickname.Equals("Velica_Foriana")) testResult = true;
             else testResult = false;
             PrintTestResult("EN Rsig Autodetect", testResult);
 
 
 
             // Test RU ratsignal
-            rsig = "RATSIGNAL Case #5 PC HOR – CMDR zzammaxx – System: \"SECTOR RU-C A14 - 2\" (Unconfirmed) – Language: Russian (Russia) (ru-RU) (HOR_SIGNAL)";
+            rsig = "RATSIGNAL Case #5 PC HOR – CMDR Delryn – System: \"SECTOR RU-C A14 - 2\" (Unconfirmed) – Language: Russian (Russia) (ru-RU) (HOR_SIGNAL)";
             ratsignal = new ChannelNormalMessageArgs(rsig, fuelratsChan);
             ratsignal.User.Nick = "MechaSqueak[BOT]";
             testPlugin.deepl_auto_case(new RegisteredCommandArgs("", fuelratsChan));
@@ -64,7 +64,7 @@ namespace adiIRC_DeepL_plugin_test
 
             // Check if case was onboarded into correct monitor_items slot
             if (testPlugin.monitor_items[5] != null && 
-                testPlugin.monitor_items[5].nickname.Equals("zzammaxx") &&
+                testPlugin.monitor_items[5].nickname.Equals("Delryn") &&
                 testPlugin.monitor_items[5].langcode.Equals("RU")) testResult = true;
             else testResult = false;
             PrintTestResult("RU Rsig Autodetect", testResult);
@@ -73,9 +73,9 @@ namespace adiIRC_DeepL_plugin_test
             // Test case remove
             testPlugin.deepl_rm(new RegisteredCommandArgs("3", fuelratsChan));
             // test case 3 should be null, and test case 5 should be unaffected
-            if (testPlugin.monitor_items[3] == null && testPlugin.monitor_items[5].nickname.Equals("zzammaxx")) testResult = true;
+            if (testPlugin.monitor_items[3] == null && testPlugin.monitor_items[5].nickname.Equals("Delryn")) testResult = true;
             else testResult = false;
-            PrintTestResult("ECase Remove", testResult);
+            PrintTestResult("Case Number Remove", testResult);
 
 
             // Test manual monitoring

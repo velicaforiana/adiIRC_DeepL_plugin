9f582a530766ef592ebc86b93787d4542c28804e
Delrynn <delryn@patton.pro>
2024-03-08 10:51:02 -0500

    Syncing TestPlugin with AdiIRCPlugin

--- TestPlugin.cs
+++ TestPlugin.cs
@@ -28,6 +28,11 @@ namespace adiIRC_DeepL_plugin_test
     {
         public string detected_source_language;
         public string text;
+        public deepl_translation()
+        {
+            detected_source_language = "";
+            text = "";
+        }
     }
 
     public class monitorItem
@@ -158,7 +163,7 @@ namespace adiIRC_DeepL_plugin_test
         /// <param name="lang">Target Language</param>
         /// <param name="totranslate">Message to translate</param>
         /// <returns></returns>
-        public async Task<string> deepl_translate_any(string lang, string totranslate, string nick="")
+        public async Task<deepl_translation> deepl_translate_any(string lang, string totranslate, string nick="")
         {
             try
             {
@@ -177,22 +182,7 @@ namespace adiIRC_DeepL_plugin_test
                     if (response.IsSuccessStatusCode)
                     {
                         deepl_json_response jsonResponse = JsonConvert.DeserializeObject<deepl_json_response>(await response.Content.ReadAsStringAsync());
-                        int index;
-                        if (IsNickMonitored(nick, out index))
-                        {
-                            if (!string.IsNullOrEmpty(nick) && jsonResponse.translations[0].detected_source_language.Equals("EN"))
-                            {
-                                monitor_items[index].retries++;
-                                if (monitor_items[index].retries >= 3)
-                                {
-                                    monitor_items[index].langcode = "EN";
-                                    PrintDebug("Set user {0} to English.", monitor_items[index].nickname);
-                                }
-                            }
-                            else
-                                monitor_items[index].retries = 0;
-                        }
-                        return jsonResponse.translations[0].text;
+                        return jsonResponse.translations[0];
                     }
                 }
             }
@@ -200,7 +190,7 @@ namespace adiIRC_DeepL_plugin_test
             {
                 adihost.ActiveIWindow.OutputText(e.ToString());
             }
-            return "";
+            return new deepl_translation();
         }
 
         /// <summary>
@@ -212,7 +202,23 @@ namespace adiIRC_DeepL_plugin_test
         /// <param name="fromNick">Client's nick</param>
         public async void deepl_translate_towindow(string lang, string totranslate, IWindow window, string fromNick)
         {
-            window.OutputText(fromNick + ": " + await deepl_translate_any(lang, totranslate, fromNick));
+            deepl_translation translation = await deepl_translate_any(lang, totranslate);
+            int index;
+            if (IsNickMonitored(fromNick, out index))
+            {
+                if (!string.IsNullOrEmpty(fromNick) && translation.detected_source_language.Equals("EN"))
+                {
+                    monitor_items[index].retries++;
+                    if (monitor_items[index].retries >= 3)
+                    {
+                        monitor_items[index].langcode = "EN";
+                        PrintDebug("Set user {0} to English.", monitor_items[index].nickname);
+                    }
+                }
+                else
+                    monitor_items[index].retries = 0;
+            }
+            window.OutputText(fromNick + "(" + translation.detected_source_language + "): " + translation.text);
         }
 
         /// <summary>
@@ -234,7 +240,8 @@ namespace adiIRC_DeepL_plugin_test
             string allarguments = argument.Command.Substring(argument.Command.IndexOf(" ") + 1);
             string lang = allarguments.Substring(0, 2).ToUpper();
             string totranslate = allarguments.Substring(3);
-            argument.Window.Editbox.Text = await deepl_translate_any(lang, totranslate);
+            deepl_translation translation = await deepl_translate_any(lang, totranslate);
+            argument.Window.Editbox.Text = translation.text;
         }
 
         /// <summary>
@@ -406,7 +413,7 @@ namespace adiIRC_DeepL_plugin_test
             adihost.ActiveIWindow.OutputText("     autoRemoveNicks  -> toggles auto removal of non-case nicks when nick parts or quits");
             adihost.ActiveIWindow.OutputText("     drillmode        -> toggles whether to observe MechaSqeak or DrillSqueak");
             adihost.ActiveIWindow.OutputText("     debugmode        -> toggles extra debug messages during operations");
-            adihost.ActiveIWindow.OutputText("/deepl-debug - Lists items monitored and/or other plugin debug information");
+            adihost.ActiveIWindow.OutputText("/dl-debug - Lists items monitored and/or other plugin debug information");
             adihost.ActiveIWindow.OutputText("/dl-help - Shows this command reference");
         }
 

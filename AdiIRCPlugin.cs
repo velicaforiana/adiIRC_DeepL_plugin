5f445ef3637c2a48e6fd784bb20a57bfe2e73347
Delrynn <33699600+Delrynn@users.noreply.github.com>
2024-03-12 14:33:08 -0400

    Undo repeat client "fix"

--- AdiIRCPlugin.cs
+++ AdiIRCPlugin.cs
@@ -521,21 +521,11 @@
                                 {
                                     string nick = match.Groups["nickname"].Value;
 
-                                    // check if repeat client
-                                    int index;
-                                    if (IsNickMonitored(nick, out index))
-                                        monitor_items[index] = null;
-
                                     monitor_items[caseNum] = new monitorItem(nick, cmdr, langcode: langcode);
                                     PrintDebug("Monitoring " + nick);
                                 }
                                 else
                                 {
-                                    // check if repeat client
-                                    int index;
-                                    if (IsNickMonitored(cmdr, out index))
-                                        monitor_items[index] = null;
-
                                     monitor_items[caseNum] = new monitorItem(cmdr, cmdr, langcode: langcode);
                                     PrintDebug("Monitoring " + cmdr);
                                 }
